c59909917f38401f1122098758a4510958d8e811
dpfaj <dpfaj321@gmail.com>
2024-09-30 13:37:54 +0900

    옷가게 할인 받기

---
+++ lv.0/d5,d6/p25.py
@@ -0,0 +1,10 @@
+#옷가게 할인 받기
+def solution(price):
+    if price >= 500000:
+        return int(price*0.8)
+    elif price >= 300000:
+        return int(price*0.9)
+    elif price >= 100000:
+        return int(price*0.95)
+    else:
+        return price
